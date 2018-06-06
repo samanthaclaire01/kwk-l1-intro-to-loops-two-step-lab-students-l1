e7e0549ecf9919613503fbb2baf4047274e2ec09
Samantha Cassity <cassityclaire@gmail.com>
2018-06-06 18:07:52 +0000

    automatically backed up by learn

---
+++ hotel_welcome.rb
@@ -0,0 +1,4 @@
+def tv_welcome(guestname, roomnumber)
+  puts "#{guestname}, welcome to the Springhill Suites! Your room number is #{roomnumber}."
+end
+tv_welcome("Sam", "321")
--- loops.rb
+++ loops.rb
@@ -1,15 +1,29 @@
-def dance 
-  moves_slay = 0 
+# def dance 
+#   moves_slay = 0 
   
-  loop do 
-    if moves_slay == 10 
+#   loop do 
+#     if moves_slay == 10 
+#       break
+#     end
+#     puts "Step to the left"
+#     puts "Step to the right"
+#     puts "kick"
+#     moves_slay = moves_slay+1 
+# end
+# end
+# dance 
+def kwk_camp
+  days = 8
+  loop do
+    if days == 0
       break
     end
-    puts "Step to the left"
-    puts "Step to the right"
-    puts "kick"
-    moves_slay = moves_slay+1 
+    days_left = days - 1
+    puts "come to camp"
+    puts "you have #{days_left} days left"
+    days = days - 1
   end
+      puts "camp is over"
 end
-dance 
-    
+  kwk_camp
+  
